de333b89c1b61ab664e2b867599f5b8b95b87b9a
msadiqov <sadiqovmezahir7@gmail.com>
2026-02-13 11:47:35 +0400

    Add files via upload

---
+++ week03/invoice/invoice.java
@@ -0,0 +1,52 @@
+package week03.invoice;
+
+public class invoice {
+
+    // instance variables
+    private String partNumber;
+    private String partDescription;
+    private int quantity;
+    private double pricePerItem;
+
+    // constructor
+    public void Invoice(String partNumber, String partDescription, int quantity, double pricePerItem) {
+        this.partNumber = partNumber;
+        this.partDescription = partDescription;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+    }
+
+    // getters
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public String getPartDescription() {
+        return partDescription;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPricePerItem() {
+        return pricePerItem;
+    }
+
+    // setters
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public void setPartDescription(String partDescription) {
+        this.partDescription = partDescription;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPricePerItem(double pricePerItem) {
+        this.pricePerItem = pricePerItem;
+    }
+}
